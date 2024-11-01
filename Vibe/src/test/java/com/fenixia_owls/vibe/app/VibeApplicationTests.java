06b998f436e1d4c35e6ebf37874d67b187615154
andres.tellez <andres.tellez@softlond.co>
2024-10-31 23:45:59 -0500

    creacion proyecto Vibe

---
+++ Vibe/src/test/java/com/fenixia_owls/vibe/app/VibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fenixia_owls.vibe.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
